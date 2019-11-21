cf65be37c53e2bbfc57e5d634e66fc06a14ef0b7
xiang <313316759@qq.com>
2019-11-21 15:29:17 +0800

    图标

--- App.vue
+++ App.vue
@@ -16,15 +16,15 @@
   @import "./common/css/reset.scss";
   @import "./common/css/class.scss";
 	/*每个页面公共css */
-  @font-face {
-    font-family: 'iconfont';  /* project id 1495979 */
-    src: url('http://at.alicdn.com/t/font_1495979_gj3et9fbpr9.eot');
-    src: url('https://at.alicdn.com/t/font_1495979_gj3et9fbpr9.eot?#iefix') format('embedded-opentype'),
-    url('https://at.alicdn.com/t/font_1495979_gj3et9fbpr9.woff2') format('woff2'),
-    url('https://at.alicdn.com/t/font_1495979_gj3et9fbpr9.woff') format('woff'),
-    url('https://at.alicdn.com/t/font_1495979_gj3et9fbpr9.ttf') format('truetype'),
-    url('https://at.alicdn.com/t/font_1495979_gj3et9fbpr9.svg#iconfont') format('svg');
-  }
+@font-face {
+  font-family: 'iconfont';  /* project id 1495979 */
+  src: url('//at.alicdn.com/t/font_1495979_gj3et9fbpr9.eot');
+  src: url('//at.alicdn.com/t/font_1495979_gj3et9fbpr9.eot?#iefix') format('embedded-opentype'),
+  url('//at.alicdn.com/t/font_1495979_gj3et9fbpr9.woff2') format('woff2'),
+  url('//at.alicdn.com/t/font_1495979_gj3et9fbpr9.woff') format('woff'),
+  url('//at.alicdn.com/t/font_1495979_gj3et9fbpr9.ttf') format('truetype'),
+  url('//at.alicdn.com/t/font_1495979_gj3et9fbpr9.svg#iconfont') format('svg');
+}
   .iconfont{
       font-family:"iconfont" !important;
       font-size:16px;font-style:normal;
